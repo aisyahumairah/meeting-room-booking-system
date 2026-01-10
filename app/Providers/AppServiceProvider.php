583bbe08a82bcc6650a77bba6e3af7194465853b
meyyy <aisyahumairah@users.noreply.github.com>
2026-01-10 12:26:46 +0800

    feat: adding https to AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -23,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->registerGates();
         $this->registerViewComposers();
+
+        if (app()->environment('local')) {
+        URL::forceScheme('https');
+    }
     }
 
     /**
